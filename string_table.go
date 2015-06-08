0c247ae3721271aada3eecb199adcaf9d6b3a6dc
Jason Coene <jcoene@gmail.com>
2015-06-08 09:21:36 -0500

    Filenames

--- string_table.go
+++ string_table.go
@@ -4,6 +4,34 @@ import (
 	"github.com/dotabuff/manta/dota"
 )
 
+func NewStringTables() *StringTables {
+	return &StringTables{
+		tables: map[string]StringTable{},
+	}
+}
+
+type StringTables struct {
+	tables map[string]StringTable
+}
+
+type StringTable struct {
+	items map[int]*dota.CDemoStringTablesItemsT
+}
+
+func (p *StringTables) onCDemoStringTables(stringTables *dota.CDemoStringTables) error {
+	tables := map[string]StringTable{}
+
+	for _, st := range stringTables.GetTables() {
+		table := StringTable{items: map[int]*dota.CDemoStringTablesItemsT{}}
+		tables[st.GetTableName()] = table
+		for n, item := range st.GetItems() {
+			table.items[n] = item
+		}
+	}
+
+	return nil
+}
+
 // Internal parser for callback OnCDemoStringTables.
 func (p *Parser) onCDemoStringTables(m *dota.CDemoStringTables) error {
 	for _, t := range m.GetTables() {
