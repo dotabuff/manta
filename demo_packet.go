19a24338be0a23f064f10d29c8300281136e02a8
Jason Coene <jcoene@gmail.com>
2015-06-16 10:34:10 -0500

    Sort incoming messages to resolve contextual dependencies.

--- demo_packet.go
+++ demo_packet.go
@@ -1,8 +1,58 @@
 package manta
 
 import (
+	"sort"
+
 	"github.com/dotabuff/manta/dota"
 )
+
+// A message that has been read from an outerMessage but not yet processed.
+type pendingMessage struct {
+	tick uint32
+	t    int32
+	buf  []byte
+}
+
+// Calculates the priority of the message. Lower is more important.
+func (m *pendingMessage) priority() int {
+	switch m.t {
+	case
+		// These messages provide context needed for the rest of the tick
+		// and should have the highest priority.
+		int32(dota.NET_Messages_net_Tick),
+		int32(dota.SVC_Messages_svc_CreateStringTable),
+		int32(dota.SVC_Messages_svc_UpdateStringTable):
+		return -10
+
+	case
+		// These messages benefit from having context but may also need to
+		// provide context in terms of delta updates.
+		int32(dota.SVC_Messages_svc_PacketEntities):
+		return 5
+
+	case
+		// These messages benefit from having as much context as possible and
+		// should have the lowest priority.
+		int32(dota.EBaseGameEvents_GE_Source1LegacyGameEvent):
+		return 10
+	}
+
+	return 0
+}
+
+// Provides a sortable structure for storing packets in the same tick.
+type pendingMessages []*pendingMessage
+
+func (ms pendingMessages) Len() int      { return len(ms) }
+func (ms pendingMessages) Swap(i, j int) { ms[i], ms[j] = ms[j], ms[i] }
+func (ms pendingMessages) Less(i, j int) bool {
+	if ms[i].tick < ms[j].tick {
+		return true
+	}
+
+	// String tables first
+	return ms[i].priority() < ms[j].priority()
+}
 
 // Internal parser for callback OnCDemoPacket, responsible for extracting
 // multiple inner packets from a single CDemoPacket. This is the main payload
@@ -10,18 +60,28 @@ import (
 func (p *Parser) onCDemoPacket(m *dota.CDemoPacket) error {
 	r := newDemoPacketReader(m.GetData())
 
+	ms := make(pendingMessages, 0)
+
+	// Collect all messages from the packet.
 	for r.hasNext() {
 		t, buf := r.readNext()
+		ms = append(ms, &pendingMessage{p.Tick, t, buf})
+	}
 
+	// Sort messages to ensure context dependencies are met.
+	sort.Sort(ms)
+
+	// Dispatch messages in order.
+	for _, m := range ms {
 		// Skip message we don't have a definition for (yet)
 		// XXX TODO: remove this when we get updated protos.
-		if t == 547 || t == 400 {
+		if m.t == 547 || m.t == 400 {
 			continue
 		}
 
 		// Call each packet, panic if we encounter an error.
 		// XXX TODO: this should return the error up the chain. Panic for debugging.
-		if err := p.CallByPacketType(t, buf); err != nil {
+		if err := p.CallByPacketType(m.t, m.buf); err != nil {
 			panic(err)
 		}
 	}
