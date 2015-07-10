3a5ec7c9914c1d6427f6cd37e8a91bacbfc4c4e8
Robin <me@invokr.org>
2015-07-10 18:28:46 +0200

    Working version of the flattened serializer tree

---
+++ flattened_serializers.go
@@ -0,0 +1,134 @@
+package manta
+
+import (
+	"os"
+
+	"github.com/dotabuff/manta/dota"
+	"github.com/golang/protobuf/proto"
+)
+
+// Field is always filled, table only for sub-tables
+type dt_property struct {
+	Field *dt_field
+	Table *dt
+}
+
+// A datatable field
+type dt_field struct {
+	Name string
+	Type string
+
+	Flags     *int32
+	BitCount  *int32
+	LowValue  *float32
+	HighValue *float32
+
+	Version *int32
+}
+
+// A single datatable
+type dt struct {
+	Name       string
+	Flags      *int32
+	Version    int32
+	Properties []*dt_property
+}
+
+// The flattened serializers object
+type flattened_serializers struct {
+	Serializers map[string]map[int32]*dt // serializer name -> [versions]
+	proto       *dota.CSVCMsg_FlattenedSerializer
+}
+
+// Fills properties for a data table
+func (sers *flattened_serializers) recurse_table(cur *dota.ProtoFlattenedSerializerT) *dt {
+	// Basic table structure
+	table := &dt{
+		Name:       sers.proto.GetSymbols()[cur.GetSerializerNameSym()],
+		Version:    cur.GetSerializerVersion(),
+		Properties: make([]*dt_property, 0),
+	}
+
+	props := sers.proto.GetFields()
+
+	// Append all the properties
+	for _, idx := range cur.GetFieldsIndex() {
+		pField := props[idx]
+		prop := &dt_property{nil, nil}
+
+		// Field can always be set
+		prop.Field = &dt_field{
+			Name: sers.proto.GetSymbols()[pField.GetVarNameSym()],
+
+			Flags:     pField.EncodeFlags,
+			BitCount:  pField.BitCount,
+			LowValue:  pField.LowValue,
+			HighValue: pField.HighValue,
+
+			Type:    (sers.proto.GetSymbols()[pField.GetVarTypeSym()]),
+			Version: pField.FieldSerializerVersion,
+		}
+
+		// Optional: Attach the serializer version for the property if applicable
+		if pField.FieldSerializerNameSym != nil {
+			pFieldName := sers.proto.GetSymbols()[pField.GetFieldSerializerNameSym()]
+			pFieldVersion := pField.GetFieldSerializerVersion()
+			pSerializer := sers.Serializers[pFieldName][pFieldVersion]
+
+			if pSerializer == nil {
+				_panicf("Error: Serializer version %d for %s hasn't been added yet.", pFieldVersion, pFieldName)
+			}
+
+			prop.Table = pSerializer
+		}
+
+		table.Properties = append(
+			table.Properties,
+			prop,
+		)
+	}
+
+	return table
+}
+
+// Internal callback for OnCDemoSendTables.
+func (p *Parser) onCDemoSendTablesNew(m *dota.CDemoSendTables) error {
+	// This packet just contains a single large buffer
+	r := newReader(m.GetData())
+
+	// The buffer starts with a varint encoded length
+	size := int(r.readVarUint32())
+	if size != r.remBytes() {
+		_panicf("expected %d additional bytes, got %d", size, r.remBytes())
+	}
+
+	// Read the rest of the buffer as a CSVCMsg_FlattenedSerializer.
+	buf := r.readBytes(size)
+	msg := &dota.CSVCMsg_FlattenedSerializer{}
+	if err := proto.Unmarshal(buf, msg); err != nil {
+		_panicf("cannot decode proto: %s", err)
+	}
+
+	// Create the flattened_serializers object and fill it
+	fs := &flattened_serializers{
+		Serializers: make(map[string]map[int32]*dt),
+		proto:       msg,
+	}
+
+	// Iterate through all flattened serializers and fill their properties
+	for _, o := range msg.GetSerializers() {
+		sName := msg.GetSymbols()[o.GetSerializerNameSym()]
+		sVer := o.GetSerializerVersion()
+
+		if fs.Serializers[sName] == nil {
+			fs.Serializers[sName] = make(map[int32]*dt)
+		}
+
+		fs.Serializers[sName][sVer] = fs.recurse_table(o)
+	}
+
+	// Exit here before work continues
+	os.Exit(0)
+
+	return nil
+}
