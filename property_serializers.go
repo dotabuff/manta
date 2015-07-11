8c8bbbaaac4a0ad506956c5b859a10b874d6bb1e
Robin <me@invokr.org>
2015-07-11 16:00:11 +0200

    Added list of types required for CWorld

---
+++ property_serializers.go
@@ -0,0 +1,40 @@
+package manta
+
+// Required for CWorld to work:
+// ----------------------------
+// - CHandle< CBaseEntity >
+// - CBodyComponent
+// - CNetworkedQuantizedFloat
+// - CGameSceneNodeHandle
+// - CStrongHandle< InfoForResourceTypeCTextureBase >
+// - CStrongHandle< InfoForResourceTypeCModel >
+// - CUtlStringToken
+// - CUtlVector< CAnimationLayer >
+// - CEntityIdentity*
+// - CUtlSymbolLarge
+// - CPhysicsComponent
+// - CRenderComponent
+//
+// - Color
+// - QAngle
+// - HSequence
+// - Vector
+// - SolidType_t
+// - SurroundingBoundsType_t
+// - MoveCollide_t
+// - MoveType_t
+// - gender_t
+// - RenderMode_t
+// - RenderFx_t
+//
+// - bool
+// - uint8
+// - uint16
+// - uint32
+// - uint64
+// - int8
+// - int32
+// - float32
+//
+// - float32[24]
+// - CStrongHandle< InfoForResourceTypeIMaterial2 >[6]
